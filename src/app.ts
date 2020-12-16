1320feb7af1a433cced051ed986d83d75d71f57d
rasatmaja <rasioatmaja29@gmail.com>
2020-12-16 13:39:41 +0700

    setup initial server

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,7 @@
  * @author Rasio Atmaja <contact@rasio.dev>
  */
 
+import Server from './server'
 
-import express, {Application, Request, Response, NextFunction} from 'express';
-
-// init express
-const app: Application = express();
-
-// setup routes 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Links Barnacle ready to serve')
-})
-
-// Setup server with port 5000
-app.listen(5000, () => console.log('Server running ...'))
+// init server
+const server = new Server();
