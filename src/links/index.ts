c594ab71fd49e86e15e832b30f9b02251ab0d9ac
rasatmaja <rasioatmaja29@gmail.com>
2020-12-16 14:44:07 +0700

    init controller for links feature

---
+++ src/links/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Links Barnacle - A restfull API that serving a data for project links.rasio.dev.
+ * 
+ * @author Rasio Atmaja <contact@rasio.dev>
+ */
+
+import { Request, Response, NextFunction} from 'express';
+class Links {
+    
+    async getLinks(req: Request, res: Response, next: NextFunction) {
+        res.send('Links Barnacle ready to serve')
+    }
+    
+}
+
+export default Links
