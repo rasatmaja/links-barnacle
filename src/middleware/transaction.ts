1753df6954ea4f08d655e848eb8f95d1e44ac4f5
rasatmaja <rasioatmaja29@gmail.com>
2020-12-17 16:55:41 +0700

    create transaction middleware

---
+++ src/middleware/transaction.ts
@@ -0,0 +1,21 @@
+import { Response, Request, NextFunction } from "express";
+import { fLog } from "../utils/log";
+import GenerateRamdomString from "../utils/random.string";
+const transactions = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const reqID = GenerateRamdomString(10, true, true, true);
+  const flog = fLog.child({
+    ctx: "middleware",
+    func: "transaction",
+    reqID: reqID,
+  });
+  flog.trace("Start Transactions");
+  req.headers["X-Request-ID"] = reqID;
+  res.setHeader("X-Request-ID", reqID);
+  next();
+  flog.trace("End Transactions");
+};
+export default transactions;
