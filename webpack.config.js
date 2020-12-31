f37a591a72940be71765f0559b742733eb7b734e
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2020-12-31 21:54:25 +0700

    😍 Fix Swagger UI (#55)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 var path = require("path");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const TerserPlugin = require("terser-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -25,4 +27,18 @@ module.exports = {
   optimization: {
     minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })], // mangle false else mysql blow ups with "PROTOCOL_INCORRECT_PACKET_SEQUENCE"
   },
+  node: {
+    __dirname: false,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        "./node_modules/swagger-ui-dist/swagger-ui.css",
+        "./node_modules/swagger-ui-dist/swagger-ui-bundle.js",
+        "./node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js",
+        "./node_modules/swagger-ui-dist/favicon-16x16.png",
+        "./node_modules/swagger-ui-dist/favicon-32x32.png",
+      ],
+    }),
+  ],
 };
